398667b614025dae8bb1dac3a60bcf232a7b09bc
0xiloveyou <gamil.iloveyou@gmail.com>
2026-06-14 19:38:52 +0600

    Data insertion & query 1

--- Query.sql
+++ Query.sql
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+
+
+
 -- =========================================================================
 -- SYSTEM: Football Ticket Booking System Database Setup Template
 
@@ -52,7 +61,7 @@ CREATE TABLE Bookings (
     seat_number VARCHAR(10),
     payment_status VARCHAR(15) 
     CHECK( payment_status in ('Pending', 'Confirmed', 'Cancelled', 'Refunded') OR payment_status IS NULL),
-    total_cost DECIMAL(10,5)
+    total_cost DECIMAL(10,5) NOT NULL
     CHECK (total_cost > 0),
     UNIQUE(match_id, seat_number)
     
@@ -62,28 +71,46 @@ CREATE TABLE Bookings (
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO USERS
 -- =========================================================================
--- INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
--- (1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
--- (2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
--- (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
--- (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO MATCHES
 -- =========================================================================
--- INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
--- (101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
--- (102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
--- (103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
--- (104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
--- (105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
+INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO BOOKINGS
 -- =========================================================================
--- INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
--- (501, 1, 101, 'A-12', 'Confirmed', 150.00),
--- (502, 1, 102, 'B-04', 'Confirmed', 120.00),
--- (503, 2, 101, 'A-13', 'Confirmed', 150.00),
--- (504, 2, 101, NULL, NULL, 150.00),
--- (505, 3, 102, 'C-20', 'Pending', 120.00);
+INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150.00),
+(502, 1, 102, 'B-04', 'Confirmed', 120.00),
+(503, 2, 101, 'A-13', 'Confirmed', 150.00),
+(504, 2, 101, NULL, NULL, 150.00),
+(505, 3, 102, 'C-20', 'Pending', 120.00);
+
+-- Query 1
+-- Retrieve all upcoming football matches belonging to the 'Champions League'
+-- where the match status is 'Available'.
+select match_id, fixture, base_ticket_price from matches where tournament_category = 'Champions League' and match_status = 'Available'
+
+-- Query 2
+
+
+-- Query 3
+
+-- Query 4
+
+-- Query 5
+
+-- Query 6
+
+-- Query 7
