9b3d719a0a298197cc0d2d99faecf8d04de74298
0xiloveyou <gamil.iloveyou@gmail.com>
2026-06-14 23:01:27 +0600

    query 5 & 6 done

--- Query.sql
+++ Query.sql
@@ -1,12 +1,3 @@
-
-
-
-
-
-
-
-
-
 -- =========================================================================
 -- SYSTEM: Football Ticket Booking System Database Setup Template
 
@@ -120,16 +111,18 @@ select booking_id, full_name, fixture, round(total_cost) from bookings
    inner join matches using(match_id)
 
 -- Query 5
---
---
+--Display a comprehensive list of all users and their booking IDs,
+-- ensuring that fans who have never bought a ticket are still listed.
 
+select user_id, full_name, booking_id
+from users full join bookings using(user_id);
 
 -- Query 6
---
---
-
+--Find all ticket bookings where the total cost is strictly 
+--higher than the average cost of all ticket bookings.
+select booking_id, match_id, total_cost 
+from bookings  where total_cost > (select round(avg(total_cost)) from bookings)
 
 -- Query 7
 --
 --
-
